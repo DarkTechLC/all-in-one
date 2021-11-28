a84532f683a7b93fdaf0174930f77258a56d4aa2
Luís Clício <cliciosa14@gmail.com>
2021-11-28 11:26:33 -0300

    feat: updates Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 
-import logo from '../assets/logo.svg';
+import { Container } from './Container';
 
 export function Navbar() {
   return (
-    <nav className="bg-gray-900 sticky top-0 py-3">
-      <Link to="/">
-        <img src={logo} alt="All in One" />
-      </Link>
+    <nav className="bg-gray-900 bg-opacity-80 backdrop-filter backdrop-blur-md sticky top-0 h-20 py-4 z-10">
+      <Container className="flex items-center justify-between h-full">
+        <Link to="/" tabIndex={0}>
+          <img src="/logo.svg" alt="All in One" className="h-10" />
+        </Link>
+      </Container>
     </nav>
   );
 }
